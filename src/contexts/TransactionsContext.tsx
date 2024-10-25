340b57a6579788421b29512305ddf4f5a58b1e2f
WilliamFilh0 <williamfilho8908@gmail.com>
2024-10-25 16:15:34 -0300

    feat: fetch transactions

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -11,27 +11,43 @@ interface Transaction {
 
 interface TransactionContextType {
   transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
 
-interface TreansactionsProviderProps {
+interface TransactionsProviderProps {
   children: ReactNode;
 }
 
 export const TransactionsContext = createContext({} as TransactionContextType);
 
-export function TransactionsProvider({ children }: TreansactionsProviderProps) {
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
+  async function fetchTransactions(query?: string) {
+    const url = new URL("http://localhost:3333/transactions");
+
+    if (query) {
+      url.searchParams.append("description", query);
+    }
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
   useEffect(() => {
-    fetch("http://localhost:3333/transactions")
-      .then((response) => response.json())
-      .then((data) => {
-        setTransactions(data);
-      });
+    fetchTransactions();
   }, []);
 
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider
+      value={{
+        transactions,
+        fetchTransactions,
+      }}
+    >
       {children}
     </TransactionsContext.Provider>
   );
